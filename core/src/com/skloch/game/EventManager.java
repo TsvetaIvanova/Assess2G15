a56f3015dabcc6f86c181273c521f87f257618e3
TsvetaIvanova <116024683+TsvetaIvanova@users.noreply.github.com>
2024-04-24 13:19:59 +0100

    pond
    pond

--- core/src/com/skloch/game/EventManager.java
+++ core/src/com/skloch/game/EventManager.java
@@ -43,6 +43,7 @@ public class EventManager {
         objectInteractions.put("accomodation", "Go to sleep for the night?\nYour alarm is set for 8am.");
         objectInteractions.put("rch", null); // Changes, dynamically returned in getObjectInteraction
         objectInteractions.put("tree", "Speak to the tree?");
+        objectInteractions.put("fishing", "Catch a fish?");
 
         // Some random topics that can be chatted about
         String[] topics = {"Dogs", "Cats", "Exams", "Celebrities", "Flatmates", "Video games", "Sports", "Food", "Fashion"};
@@ -53,12 +54,17 @@ public class EventManager {
         String[] args = eventKey.split("-");
 
         // Important functions, most likely called after displaying text
-        if (args[0].equals("fadefromblack")) {
+        if (args[0] == "fadefromblack") {
             fadeFromBlack();
-        } else if (args[0].equals("fadetoblack")) {
+        } else if (args[0] == "fadetoblack") {
             fadeToBlack();
-        } else if (args[0].equals("gameover")) {
+        } else if (args[0] == "gameover") {
             game.GameOver();
+        }
+
+        if (eventKey.equals("fishing")) {
+            fishingEvent();
+            return;
         }
 
         // Events related to objects
@@ -134,6 +140,15 @@ public class EventManager {
     public void objectEvent(String object) {
         game.dialogueBox.hideSelectBox();
         game.dialogueBox.setText("This is a " +  object + "!");
+    }
+
+    /**
+     * Lets the player catch a fish!
+     */
+    public void fishingEvent() {
+        game.dialogueBox.show();
+        game.dialogueBox.setText("You caught a fish!");
+        game.decreaseEnergy(20);
     }
 
     /**
