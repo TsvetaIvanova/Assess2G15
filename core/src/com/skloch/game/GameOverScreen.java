ef6b16ece0ecccabc2f9771c78b8cbc3399b1df4
aj1537 <apriljack333@gmail.com>
2024-05-08 11:52:01 +0100

    achievement score table alignment stuff

--- core/src/com/skloch/game/GameOverScreen.java
+++ core/src/com/skloch/game/GameOverScreen.java
@@ -105,11 +105,9 @@ public class GameOverScreen implements Screen {
 
 
 
-
         // Achievements scores table setup
         Table achievementsScoresTable = new Table();
-        achievementsTable.add(achievementsScoresTable).prefHeight(300).prefWidth(300);
-        achievementsTable.add(achievementsScoresTable).expand().fill();
+        achievementsTable.add(achievementsScoresTable).prefHeight(300).prefWidth(300).expand().fill();
         achievementsTable.row();
 
 
@@ -123,34 +121,33 @@ public class GameOverScreen implements Screen {
 
         // Track if any achievements have been added
         boolean anyAchievements = false;
-
         // Populate the table with achievements
         if (isBestFisher) {
             anyAchievements = true;
             Label bestFisherLabel = new Label("BestFisher: Yayy, you caught enough fish to give to your friends!", descriptionStyle);
             bestFisherLabel.setWrap(true);
-            achievementsScoresTable.add(bestFisherLabel).width(240).padBottom(10).padLeft(10).padRight(30);
+            achievementsScoresTable.add(bestFisherLabel).width(240).padBottom(20).padLeft(35).padRight(30);
             achievementsScoresTable.row();
         }
         if (isBookworm) {
             anyAchievements = true;
             Label bookwormLabel = new Label("Bookworm: You spent an impressive amount of time in the software lab doing your ENG1 project!", descriptionStyle);
             bookwormLabel.setWrap(true);
-            achievementsScoresTable.add(bookwormLabel).width(240).padBottom(10).padLeft(10).padRight(30);
+            achievementsScoresTable.add(bookwormLabel).width(240).padBottom(20).padLeft(35).padRight(30);
             achievementsScoresTable.row();
         }
         if (isDuckDuckGo) {
             anyAchievements = true;
             Label duckDuckGoLabel = new Label("Duck duck go: What a hero! Single-handedly taking care of the duck population on campus!", descriptionStyle);
             duckDuckGoLabel.setWrap(true);
-            achievementsScoresTable.add(duckDuckGoLabel).width(245).padBottom(10).padLeft(10).padRight(30);
+            achievementsScoresTable.add(duckDuckGoLabel).width(245).padBottom(20).padLeft(35).padRight(30);
             achievementsScoresTable.row();
         }
 
         if (!anyAchievements) {
             Label noAchievLabel = new Label("No achievements this game:(", descriptionStyle);
             noAchievLabel.setWrap(true);
-            achievementsScoresTable.add(noAchievLabel).width(245).height(300).padBottom(10).padLeft(10).padRight(30);
+            achievementsScoresTable.add(noAchievLabel).width(245).height(300).padBottom(20).padLeft(35).padRight(30);
             achievementsScoresTable.row();
         }
 
@@ -158,10 +155,9 @@ public class GameOverScreen implements Screen {
         if (bonusStreaks > 0) {
             Label bonusStreaksLabel = new Label("Bonus Streaks: " + bonusStreaks + " points", descriptionStyle);
             bonusStreaksLabel.setWrap(true);
-            achievementsScoresTable.add(bonusStreaksLabel).width(240).padTop(20).padBottom(10).padLeft(10).padRight(30);
+            achievementsScoresTable.add(bonusStreaksLabel).width(240).padTop(20).padBottom(10).padLeft(35).padRight(30);
             achievementsScoresTable.row();
         }
-
 
         // Pack to apply sizes and positions
         achievementsTable.pack();
