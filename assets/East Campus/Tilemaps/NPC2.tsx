6a745725e636c0d5b08c99d83331a319ccb8f342
Adam Robinson <ar1883york.ac.uk>
2024-05-17 21:50:27 +0100

    Added Jerry and Tilly. Jerry dropped his phone in the lake and Tilly is narcoleptic.
    Also the signs don't get in people's way anymore.

---
+++ assets/East Campus/Tilemaps/NPC2.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="NPC2" tilewidth="24" tileheight="24" tilecount="4" columns="1">
+ <image source="NPC2.png" width="24" height="96"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="500"/>
+   <frame tileid="2" duration="500"/>
+  </animation>
+ </tile>
+ <tile id="1">
+  <animation>
+   <frame tileid="1" duration="500"/>
+   <frame tileid="3" duration="500"/>
+  </animation>
+ </tile>
+</tileset>
